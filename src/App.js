91bf048fc22bdcfd63e32776d3e9e3ba5ba79ca7
normanfung <56857601+normanfung@users.noreply.github.com>
2020-08-12 13:57:48 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import React from "react";
+import { Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-const HomePage = props => {
+const HomePage = (props) => {
   console.log(props);
   return (
     <div>
-      <button onClick={() => props.history.push('/topics')}>Topics </button>
+      <button onClick={() => props.history.push("/topics")}>Topics</button>
       <h1>HOME PAGE</h1>
     </div>
   );
 };
 
-const TopicsList = props => {
+const TopicsList = (props) => {
   return (
     <div>
       <h1>TOPIC LIST PAGE</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+      <Link to={`${props.match.url}/13`}>To 13</Link>
+      <Link to={`${props.match.url}/14`}>To 14</Link>
+      <Link to={`${props.match.url}/15`}>To 15</Link>
     </div>
   );
 };
 
-const TopicDetail = props => {
+const TopicDetail = (props) => {
+  console.log(props.match.params.topicId);
   return (
     <div>
-      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+      <h1>TOPIC DETAIL PAGE {props.match.params.topicId}</h1>
     </div>
   );
 };
@@ -35,11 +36,9 @@ const TopicDetail = props => {
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/blog/asdqw/topics' component={TopicsList} />
-      <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} />
-      <Route exact path='/blog/topics' component={TopicsList} />
-      <Route path='/blog/topics/:topicId' component={TopicDetail} />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/blog/asdawsds/topics" component={TopicsList} />
+      <Route path="/blog/asdawsds/topics/:topicId" component={TopicDetail} />
     </div>
   );
 }
